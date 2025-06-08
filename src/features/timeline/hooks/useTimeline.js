dc269a2d3f9a5d41979003d055faaade2c3fc37a
pjw7536 <pjw7536@gmail.com>
2025-06-08 13:36:18 +0900

    re

--- src/features/timeline/hooks/useTimeline.js
+++
@@ -1,8 +0,0 @@
-import { useRef } from "react";
-import { useTimelineRenderer } from "./useTimelineRenderer";
-
-export const useTimeline = (groupKey, data, options) => {
-  const ref = useRef(null);
-  useTimelineRenderer(ref, groupKey, data, options);
-  return ref;
-};
